3a6231be4a8a1c38c67d4c54c2a39ba2964c2c20
jatin-codes-sketch <jkthakur081@gmail.com>
2025-05-31 19:05:06 +0530

    solve the leet code problem - Sort color

---
+++ Linear-Structure/vector/11.leet_code_Sort_Color.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void SortColor(vector<int>&vec){
+    int low=0,mid=0,large=vec.size();
+    
+    while(mid<=large){
+        
+        // when value will be 0
+        if(vec[mid]==0){
+            swap(vec[low],vec[mid]);
+            low++;
+            mid++;
+        }else if(vec[mid]==1){
+            mid++;
+        }else{
+            swap(vec[mid],vec[large]);
+            large--;
+        }
+        
+    }
+    
+}
+
+
+int main(){
+    
+    vector<int>vec={2,0,2,1,2,1,0,1,2,2,0};
+    int n=vec.size();
+    
+    cout<<"the array is ";
+    
+    
+    for(int i=0;i<n;i++){
+        cout<<vec[i]<<" ";
+    }cout<<endl;
+    
+    SortColor(vec);
+    
+    cout<<"the sorted array is ";
+    for(int i=0;i<n;i++){
+        cout<<vec[i]<<" ";
+    }
+    
+    
+    return 0;
+}
