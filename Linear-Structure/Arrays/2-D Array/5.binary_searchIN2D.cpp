8ce0cb452689de0cac69338661017dbe3cfd8708
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-10 15:46:23 +0530

    learn binary search

--- Linear-Structure/Arrays/2-D Array/5.binary_searchIN2D.cpp
+++ Linear-Structure/Arrays/2-D Array/5.binary_searchIN2D.cpp
@@ -1,6 +1,39 @@
 #include<iostream>
+#include<vector>
 using namespace std;
 
+bool search_in_2D_array(vector<vector<int>>mat,int target){
+    int m=mat.size(),n=mat[0].size();
+    int sR=0,eR=m-1;
+    while(sR<=eR){
+        int midR=sR+(eR-sR)/2;
+        if(mat[midR][n-1]>=target && mat[midR][0]<target){
+            int start=mat[midR][0],end=mat[midR][n-1];
+            while(start<=end){
+                int mid=start+(end-start)/2;
+                if(mat[midR][mid]==target)return true;
+                if(mat[midR][mid]>target)return end=mid-1;
+                else start=mid+1;
+            }
+        }
+        if(mat[midR][n-1]<target)sR=midR+1;
+        else eR=midR-1;
+    }
+    return false;
+}
+
 int main(){
+    
+    vector<vector<int>>mat={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20}};
+    
+    int target=22;
+    
+    if(search_in_2D_array(mat,target)){
+        cout<<"the target is in matrix"<<endl;
+    }else{
+        cout<<"target is not in matrix"<<endl;
+    }
+    
+    
     return 0;
 }
