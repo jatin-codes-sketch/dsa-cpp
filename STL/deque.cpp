6ff88c7152cb72958142268afa2d9f2a62f1511b
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-11 17:40:28 +0530

    update the code

--- STL/deque.cpp
+++ STL/deque.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
-#include<deque>
+#include<deque>  // double ended queue
 using namespace std;
 
 int main(){
     deque<int>d={1,2,3};
     d.push_back(4);
-    d.emplace_back(5);
-    d.emplace_front(0);
+    d.emplace_back(5);   // for adding the element at back
+    d.emplace_front(0);  // for adding the element at front 
     d.push_front(-1);
 
     for(int val:d){
         cout<<val<<" ";
     }cout<<endl;
 
-    d.pop_back();
-    d.pop_front();
+    d.pop_back(); // for pop the element at back
+    d.pop_front();  // for pop out the element at front
 
     for(int val:d){
         cout<<val<<" ";
