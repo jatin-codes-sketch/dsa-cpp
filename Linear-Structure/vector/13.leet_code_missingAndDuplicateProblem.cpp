217789d6045173de74884577dfea120afda677dd
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-12 22:18:20 +0530

    mising and duplicate leet code problem

---
+++ Linear-Structure/vector/13.leet_code_missingAndDuplicateProblem.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+using namespace std;
+
+vector<int> missingAndDuplicate(vector<vector<int>>&grid){
+    vector<int>ans;
+    unordered_set<int>s;
+    int n=grid.size();
+    int a,b;
+
+    int originalSum,sum;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            sum+=grid[i][j];
+            if(s.find(grid[i][j])!=s.end()){
+                a=grid[i][j];
+                ans.push_back(grid[i][j]);
+                break;
+            }s.insert(grid[i][j]);
+        }
+    }
+    originalSum=(n*n)*(n*n+1)/2;
+    b=originalSum+a-sum;
+    ans.push_back(b);
+
+    return ans;
+}
