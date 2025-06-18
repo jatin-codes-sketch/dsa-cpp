508863d95b74b45ece0fb98804fefc845f5187f0
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-18 17:05:34 +0530

    binary search using recursion

---
+++ Linear-Structure/Recursion/binary_search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int BS(vector<int>&arr,int n,int target,int start,int end){
+    if(start<=end){
+        int mid=start+(end-start)/2;
+        if(arr[mid]==target)return mid;
+        if(arr[mid]>target)return BS(arr,n,target,start,mid-1);
+        if(arr[mid<target])return BS(arr,n,target,mid+1,end);
+    }
+    return -1;
+}
+
+int binarySearch(vector<int>&arr,int n,int target){
+    int start=0,end=n-1;
+    
+    return BS(arr,n,target,start,end);
+}
+
+
+int main() {
+    vector<int>arr={2,3,4,5,6,7,9,11,12,13};
+    int n=arr.size();
+    int target=9;
+    
+    cout<<binarySearch(arr,n,target);
+    
+    
+    
+    
+    return 0;
+}
