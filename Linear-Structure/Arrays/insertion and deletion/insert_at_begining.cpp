a5e91e7f131577a2c8da02ad290a8b84e209caea
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-23 21:58:49 +0530

    add the delete at beginig program

--- Linear-Structure/Arrays/insertion and deletion/insert_at_begining.cpp
+++ Linear-Structure/Arrays/insertion and deletion/insert_at_begining.cpp
@@ -1,48 +1,54 @@
 #include<iostream>
 using namespace std;
 
-int capacity=5;
+int capacity = 5;
 
-bool arrFull(int size){
-    return capacity==size-1;
+bool arrFull(int size) {
+    return capacity == size;
 }
 
-bool arrEmpty(int size){
-    return size==0;
+bool arrEmpty(int size) {
+    return size == 0;
 }
 
-void insertAtBegin(int arr[],int val,int size){
-    if(arrFull(size)){
-        cout<<"array is already full cannot insert"<<endl;
+void insertAtBegin(int arr[], int val, int &size) {
+    if (arrFull(size)) {
+        cout << "Array is already full, cannot insert" << endl;
         return;
     }
-    
-    if(arrEmpty(size)){
-        arr[0]=val;
-        size++;
-        return;
+
+    for (int i = size; i > 0; i--) {
+        arr[i] = arr[i - 1];
     }
-    
-    for(int i=size-1;i>=0;i--){
-        arr[i]=arr[i-1];
-    }
-    arr[0]=val;
+
+    arr[0] = val;
     size++;
-    
-    
 }
 
-int main(){
-    
-    int arr[5]={1,3,5};
-    int size=3;
-    
-    insertAtBegin(arr,12,size);
-    
-    for(int i:arr){
-        cout<<i<<" ";
-    }cout<<endl;
-    
-    
+void deleteAtBegin(int arr[], int &size) {
+    if (arrEmpty(size)) {
+        cout << "Array is already empty" << endl;
+        return;
+    }
+
+    for (int i = 0; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    size--;
+}
+
+int main() {
+    int arr[5] = {1, 3, 5};
+    int size = 3;
+
+    insertAtBegin(arr, 12, size); // Inserting 12 at beginning
+    deleteAtBegin(arr, size);     // Deleting from beginning
+
+    for (int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
     return 0;
 }
