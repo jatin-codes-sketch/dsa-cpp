eb69e673cd20a587805fdae4d3f5cee637543f46
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-24 22:57:37 +0530

    solve the leet code suoku solver problem

---
+++ Linear-Structure/Recursion/leet_code_sudoku_solver.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+bool isSafe(vector<vector<char>>&board,int row,int col,char dig){
+    
+    // horizontal 
+    for(int j=0;j<9;j++){
+        if(board[row][j]==dig)return false;
+    }
+    
+    // vertical
+    for(int i=0;i<9;i++){
+    if(board[i][col]==dig)return false;
+}
+
+    // grid
+    int srow=(row/3)*3;
+    int scol=(col/3)*3;
+    
+    for(int i=srow;i<=srow+2;i++){
+        for(int j=scol;j<=scol+2;j++){
+            if(board[i][j]==dig)return false;
+        }
+    }
+    
+    return true;
+}
+    
+
+
+bool helper(vector<vector<char>>&board,int row,int col){
+    
+    // base case
+    if(row==9){
+        return true;
+    }
+    
+    
+    int nextRow=0,nextCol=col+1;
+    if(nextCol==9){
+        nextRow=row+1;
+        nextCol=0;
+    }
+    
+    if(board[row][col]!='.'){
+        return helper(board,nextRow,nextCol);
+    }
+    
+    for(char dig='1';dig<='9';dig++){
+        if(isSafe(board,row,col,dig)){
+            board[row][col]=dig;
+            helper(board,nextRow,nextCol);
+            return true;
+        }
+        board[row][col]='.';
+    }
+    return false;
+}
+
+void solveSudoku(vector<vector<char>>&board){
+    
+    helper(board,0,0);
+    
+    
+    
+    }
+    
+    
+    
+
+
+
