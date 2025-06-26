8bc46c8ccadc55cce51583129f4dfc1dcd5a992c
jatin-codes-sketch <jkthakur081@gmail.com>
2025-06-26 22:56:16 +0530

    solve leet code sum combination problem

---
+++ Linear-Structure/Recursion/leet_code_sum_combination.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include<set>
+#include<vector>
+using namespace std;
+
+    set<vector<int>>s;
+
+void helper(vector<int>&candidate,vector<vector<int>>&ans,vector<int>&combin,int target,int i){
+    
+    
+    if(target<0 || i==candidate.size())return;
+    
+    if(target==0){
+        if(s.find(combin)==s.end()){
+            ans.push_back(combin);
+            s.insert(combin);
+        }
+        return;
+    }
+    
+    
+    
+    combin.push_back(candidate[i]);
+    // single
+    helper(candidate,ans,combin,target-candidate[i],i+1);
+    helper(candidate,ans,combin,target-candidate[i],i); // mulitple
+    combin.pop_back();
+    
+    // no change 
+    helper(candidate,ans,combin,target,i+1);
+    
+    
+}
+
+
+vector<vector<int>> Combination(vector<int>&candidate,int target){
+    vector<vector<int>>ans;
+    vector<int>combin;
+    
+    helper(candidate,ans,combin,target,0);
+    return ans;
+}
+
+
+int main() {
+    
+    
+
+    return 0;
+}
