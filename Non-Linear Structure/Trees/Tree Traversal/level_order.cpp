282ce6e8b1f42131a311baf04828361f1c96d90b
jatin-codes-sketch <jkthakur081@gmail.com>
2025-08-04 23:02:18 +0530

    code for level order traversal

---
+++ Non-Linear Structure/Trees/Tree Traversal/level_order.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* left;
+    Node* right;
+    
+    Node(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void levelOrderTraversalTree(Node* root) {
+    if (root == nullptr) return;
+
+    queue<Node*> q;
+    q.push(root);
+    
+    while (!q.empty()) {
+        Node* curr = q.front();
+        q.pop();
+        
+        cout << curr->val << " ";
+        
+        if (curr->left) q.push(curr->left);
+        if (curr->right) q.push(curr->right);
+    }
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    
+    cout << "Level Order Traversal: ";
+    levelOrderTraversalTree(root);
+    cout << endl;
+
+    return 0;
+}
