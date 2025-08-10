7291efd827b285d221680724b86f4ded4a015985
jatin thakur <jkthakur081@gmail.com>
2025-08-10 15:38:05 +0530

    update and solve the leetcode max width of tree problem

--- Non-Linear Structure/Trees/leetcode/max_width_of_tree.cpp
+++ Non-Linear Structure/Trees/leetcode/max_width_of_tree.cpp
@@ -41,3 +41,15 @@ int maxWidth(Node* root) {
     }
     return maxWid;
 }
+
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    cout << maxWidth(root) << endl;
+    return 0;
+}
