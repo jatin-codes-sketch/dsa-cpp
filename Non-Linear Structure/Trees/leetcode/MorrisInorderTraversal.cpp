d020837fd6941c76fd0cec1912bef09c8bcb8f34
jatin thakur <jkthakur081@gmail.com>
2025-08-11 14:02:15 +0530

    solve the leetcode Inorder Traversal problem

--- Non-Linear Structure/Trees/leetcode/MorrisInorderTraversal.cpp
+++ Non-Linear Structure/Trees/leetcode/MorrisInorderTraversal.cpp
@@ -2,6 +2,13 @@
 #include<vector>
 using namespace std;
 
+struct Node{
+    int val;
+    Node* left;
+    Node* right;
+    Node(int x): val(x), left(nullptr), right(nullptr) {}
+};
+
 void helper(Node* curr,vector<int>&ans){
     while(curr!=nullptr){
         if(curr->left==nullptr){
@@ -12,7 +19,6 @@ void helper(Node* curr,vector<int>&ans){
             while(IP->right!=nullptr && IP->right!=curr){
                 IP=IP->right;
             }
-            
             if(IP->right==nullptr){
                 IP->right=curr;
                 curr=curr->left;
@@ -28,11 +34,18 @@ void helper(Node* curr,vector<int>&ans){
 vector<int> MorrisInorderTraversal(Node* root){
     vector<int>ans;
     Node* curr=root;
-    
     helper(curr,ans);
     return ans;
 }
 
 int main(){
+    Node* root=new Node(4);
+    root->left=new Node(2);
+    root->right=new Node(5);
+    root->left->left=new Node(1);
+    root->left->right=new Node(3);
+    vector<int> res=MorrisInorderTraversal(root);
+    for(int v:res) cout<<v<<" ";
+    cout<<endl;
     return 0;
 }
