efb654bfac1dfe332e4f08fc111e580801b7189d
jatin thakur <jkthakur081@gmail.com>
2025-08-12 12:16:04 +0530

    update the code for all the test cases

--- Non-Linear Structure/Trees/leetcode/flatten_BT_in_linklist.cpp
+++ Non-Linear Structure/Trees/leetcode/flatten_BT_in_linklist.cpp
@@ -1,28 +1,40 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public:
-    int value;
+struct Node {
+    int val;
     Node* left;
     Node* right;
-    
-    Node(int val):value(val),left(nullptr),right(nullptr){};
-}
+    Node(int val) : val(val), left(nullptr), right(nullptr) {}
+};
 
-Node* nextRight=nullptr;
-void flatten(Node* root){
-    if(root==nullptr)return;
-    
+Node* nextRight = nullptr;
+
+void flatten(Node* root) {
+    if (root == nullptr) return;
     flatten(root->right);
     flatten(root->left);
-    
-    root->left=nullptr;
-    root->right=nextRight;
-    nextRight=root;
+    root->left = nullptr;
+    root->right = nextRight;
+    nextRight = root;
 }
 
-int main(){
-    return 0;
+void printRightList(Node* root) {
+    while (root) {
+        cout << root->val << " ";
+        root = root->right;
+    }
+    cout << "\n";
+}
+
+int main() {
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(5);
+    root->left->left = new Node(3);
+    root->left->right = new Node(4);
+    root->right->right = new Node(6);
+
+    flatten(root);
+    printRightList(root);
 }
